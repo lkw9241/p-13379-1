5c2b63d9e5800157e5d774d56ecda676602531e1
LeeKW <lkw9241@gmail.com>
2025-06-05 14:49:44 +0900

    2+2 ==4 red

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -17,4 +17,10 @@ public class CalcTest {
     void t2() {
         assertThat(Calc.run("2 + 1")).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+    }
 }
